19ba777745386daeb5b2ff4b50f8e60faa532999
MrArtga <artga03@gmail.com>
2021-12-02 20:37:20 +0300

    new update

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -6,66 +6,173 @@ namespace DZ2
     {
         static void Main(string[] args)
         {
+            Kadrovik kadrovik = new Kadrovik("Третьяков Роберт Фёдорович");
+            Console.WriteLine(kadrovik.GetName());
+            Console.WriteLine(kadrovik.GetDoljnost() + "\n");
 
-            //Кадровик
-            Kadrovik kadrovik = new Kadrovik("Вася", "кадровик", "создавать студентов и учителей");
-            string NameKad = kadrovik.ShowName("Меня зовут ");
-            string OrientKad = kadrovik.ShowOrientation("Моя должность - ");
-            string OccupKad = kadrovik.ShowOccupation("По скольку я кадровик, я могу ");
-            Console.WriteLine(NameKad);
-            Console.WriteLine(OrientKad);
-            Console.WriteLine(OccupKad);
+            Student student = kadrovik.GetCosdaetStudent("Смирнов Борис Львович", "3-1П9");
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetZayavlen());
+            Console.WriteLine(student.GetGroup() + "\n");
 
-            var newPerson1 = kadrovik.CreateStudent("Галкин Артём Дмитриевич", "студент", "учиться");
-            Console.Write($"Меня зовут {newPerson1.Name}, и я {newPerson1.Orientation}, моя задача {newPerson1.Occupation}");
+            Teacher newTeacher = kadrovik.GetCosdaetTeacher("Игнатьева Татьяна Сергеевна", Rank.Assistant);
+            Console.WriteLine(newTeacher.GetName());
+            Console.WriteLine(newTeacher.GetDoljnost() + "\n");
 
-            Console.WriteLine("");
-
-            var newPerson2 = kadrovik.CreateTeacher("Клим Саныч", "учитель", "истории");
-            Console.Write($"Меня зовут {newPerson2.Name}, я ваш новый {newPerson2.Orientation} по {newPerson2.Occupation}");
+            Teacher teacher = new Teacher("Пономаренко Альберт Александрович", Rank.StLecturer);
+            Console.WriteLine(teacher.GetName());
+            Console.WriteLine(teacher.GetDoljnost());
+            Console.WriteLine(teacher.GetLekcee() + "\n");
         }
     }
 }
 
-
-interface IRabota
+/// <summary>
+/// Интерфейс, задающий метод для получения имени
+/// </summary>
+interface IName
 {
-    string Orientation { get; set; }                 //Должность
-    string Occupation { get; set; }                  //Род деятельности каждой из сущностей
-    string ShowOrientation(string message);          //Сообщить должность
-    string ShowOccupation(string message);           //Сообщить род деятельности
+    string GetName();
 }
 
-abstract class Person : IRabota
+/// <summary>
+/// Интерфейс, задающий метод для получения должности сотрудника
+/// </summary>
+interface IDoljnost : IName
 {
-    public string Name { get; set; } //ФИО
-    public string Orientation { get; set; }
-    public string Occupation { get; set; }
+    string GetDoljnost();
+}
 
-    public Person(string name, string orientation, string occupation)
+/// <summary>
+/// Интерфейс, задающий метод для получения учебной группы студента
+/// </summary>
+interface IGroup : IName
+{
+    string GetGroup();
+}
+
+/// <summary>
+/// Интерфейс, задающий метод для подания заявления об отчислении студента
+/// </summary>
+interface IZayava : IGroup
+{
+    string GetZayavlen();
+}
+
+/// <summary>
+/// Интерфейс, задающий метод для создания студентов и преподавателей (учителей)
+/// </summary>
+interface ICosdaet : IDoljnost
+{
+    Student GetCosdaetStudent(string name, string group);
+    Teacher GetCosdaetTeacher(string name, Rank doljnost);
+}
+
+/// <summary>
+/// Перечисление для задания ранга преподавателя
+/// </summary>
+public enum Rank
+{
+    Assistant = 0,
+    StLecturer = 1
+}
+
+/// <summary>
+/// Интерфейс, задающий метод для проведения лекций преподавателем
+/// </summary>
+interface ILekcee : IDoljnost
+{
+    string GetLekcee();
+}
+
+/// <summary>
+/// Абстрактный класс для создания людей
+/// </summary>
+abstract class Person : IName
+{
+    public string Name { get; set; }
+
+    public Person(string name)
     {
         Name = name;
-        Orientation = orientation;
-        Occupation = occupation;
     }
 
-    public virtual string ShowName(string mes) { return mes + Name; }
-    public virtual string ShowOrientation(string message) { return message + $"{Orientation}"; }
-    public virtual string ShowOccupation(string message) { return message + $"{Occupation}"; }
+    public string GetName()
+    {
+        return Name;
+    }
 }
 
-class Student : Person
+/// <summary>
+/// Абстрактный класс, описывающий сотрудников
+/// </summary>
+abstract class Sotrudnik : Person
 {
-    public Student(string name, string orientation, string occupation) : base(name, orientation, occupation) { }
+    public string Doljnost { get; set; }
+
+    public Sotrudnik(string name, string doljnost) : base(name)
+    {
+        Doljnost = doljnost;
+    }
+
+    public string GetDoljnost()
+    {
+        return Doljnost;
+    }
 }
-class Teacher : Person
+
+/// <summary>
+/// Класс, описывающий студентов
+/// </summary>
+class Student : Person, IZayava
 {
-    public Teacher(string name, string orientation, string occupation) : base(name, orientation, occupation) { }
+    public string Group { get; set; }
+
+    public Student(string name, string group) : base(name)
+    {
+        Group = group;
+    }
+
+    public string GetGroup()
+    {
+        return Group;
+    }
+
+    public string GetZayavlen()
+    {
+        return $"Заявыление на отчисление на имя: {Name}";
+    }
 }
-class Kadrovik : Person
+
+/// <summary>
+/// Класс, описывающий преподавателей (учителей)
+/// </summary>
+class Teacher : Sotrudnik, ILekcee
 {
-    public Kadrovik(string name, string orientation, string occupation) : base(name, orientation, occupation) { }
-    //Создание студентов и учителей
-    public Student CreateStudent(string name, string orientation, string occupation) { return new Student(name, orientation, occupation);  }
-    public Teacher CreateTeacher(string name, string orientation, string occupation) { return new Teacher(name, orientation, occupation); }
+    static string[] doljnost = new string[] { "Ассистент", "Старший преподаватель"};
+
+    public Teacher(string name, Rank RankOfTheTeacher) : base(name, doljnost[(int)RankOfTheTeacher]) { }
+
+    public string GetLekcee()
+    {
+        return "Проводит лекции";
+    }
+}
+
+/// <summary>
+/// Класс, описывающий кадровиков
+/// </summary>
+class Kadrovik : Sotrudnik, ICosdaet
+{
+    public Kadrovik(string name) : base(name, "кадровик") { }
+
+    public Student GetCosdaetStudent(string name, string group)
+    {
+        return new Student(name, group);
+    }
+
+    public Teacher GetCosdaetTeacher(string name, Rank doljnost)
+    {
+        return new Teacher(name, doljnost);
+    }
 }
